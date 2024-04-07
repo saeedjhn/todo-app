6d01a1b23538b1d41948b8c546e643b82f8dbdec
saeedjhn <saeedjahanakbari@gmail.com>
2024-04-08 00:46:05 +0330

    refactor: The defination constant another folder

---
+++ constant/command_mode.go
@@ -0,0 +1,10 @@
+package constant
+
+const (
+	CreateTask     = "create-task"
+	ListTask       = "list-task"
+	CreateCategory = "create-category"
+	RegisterUser   = "register-user"
+	LoginUser      = "login-user"
+	Exit           = "exit"
+)
